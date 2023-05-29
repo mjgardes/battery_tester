30f0430c88ba20036a6272604c3ff73a85098703
Matt G <garmat@gmail.com>
2023-05-29 15:36:05 -0400

    crude procedure

--- capacity_test.py
+++ capacity_test.py
@@ -16,10 +16,10 @@ from pymeasure.experiment import IntegerParameter, FloatParameter, Parameter
 class RandomProcedure(Procedure):
 
     nominal_capacity = FloatParameter('Nominal', units='Ah', default=17)
-    charge_rate = FloatParameter('c_up', units='C', default=0.2)
-    discharge_rate = FloatParameter('c_down', units='C', default=0.2)
-    charge_voltage = FloatParameter('v_charged', units='V', default=4.00)
-    discharge_voltage = FloatParameter('v_discharged', units='V', default=3.65)
+    charge_rate = FloatParameter('Charge rate', units='C', default=0.2)
+    discharge_rate = FloatParameter('Discharge rate', units='C', default=0.2)
+    charge_voltage = FloatParameter('Charge limit', units='V', default=4.00)
+    discharge_voltage = FloatParameter('Discharge limit', units='V', default=3.65)
 
     DATA_COLUMNS = ['Time', 'Voltage', 'Current', 'Charge', 'Ah_V', 'SoC']
 
@@ -31,55 +31,76 @@ class RandomProcedure(Procedure):
         self.boss.baud_rate = 9600
 
         log.info('Set Backtalk 0')
-        self.boss.write('SB0') 
-        while self.boss.read() != 'ok':
-            log.info('Didn\'t see \'ok\' yet')
+        self.boss.write('SB0')
 
         log.info('Setting Remote mode')
         self.boss.write('SR')
-        log.info(self.boss.read())
+        log.info(self.boss.read_raw())
+        log.info(self.boss.read_raw())
 
         log.info('Program limit to 1/4 ful scale (0x40)')
         self.boss.write('PL+40')
-        log.info(self.boss.read())
+        log.info(self.boss.read_raw())
+        log.info(self.boss.read_raw())
 
         log.info('Set current control mode')
         self.boss.write('SI')
-        log.info(self.boss.read())
+        log.info(self.boss.read_raw())
+        log.info(self.boss.read_raw())
 
         log.info('Zero setpoint')
         self.boss.write('PC0')
-        log.info(self.boss.read())
+        log.info(self.boss.read_raw())
+        log.info(self.boss.read_raw())
 
     def execute(self):
         log.info('Discharging at 3.4A')
         self.boss.write('PC-3.400')
-        log.info(self.boss.read())
+        log.info(self.boss.read_raw())
+        log.info(self.boss.read_raw())
 
         test_start_time = perf_counter()
+        time_elapsed = 0
         last_time = test_start_time
-        log.info(test_start_time)
-        timeout_seconds = 10#*3600
+        log.info("Clock time: %f" % test_start_time)
+        timeout_seconds = 6*3600
         charge = 0.0
+        last_voltage = 0.0
+
+        buffer_empty = False
+        while buffer_empty == False:
+            try:
+                log.info(self.boss.read())
+            except:
+                log.info('Buffer empty')
+                buffer_empty = True
+            else:
+                log.info('Purged 1 line from buffer')
+                log.info('Residue in buffer')
 
         while time_elapsed < timeout_seconds:
-            voltage = self.boss.query_ascii_values('MV')
-            current = self.boss.query_ascii_values('MI')
+            voltage = self.boss.query_ascii_values('MV')[0]
+            log.info(self.boss.read())
+            current = self.boss.query_ascii_values('MI')[0]
+            log.info(self.boss.read())
 
             time_elapsed = perf_counter() - test_start_time
             time_interval = time_elapsed - last_time
-            delta_charge = time_interval / 3600 * current
+            delta_charge = current * time_interval / 3600
             charge += delta_charge
             last_time = time_elapsed
             delta_voltage = voltage - last_voltage
-            last_voltage = voltage
+            if delta_voltage == 0.0:
+                Ah_V = np.nan
+            else:
+                Ah_V = delta_charge/delta_voltage
 
             data = {
                 'Time': time_elapsed,
                 'Voltage': voltage,
                 'Current': current,
                 'Charge': charge,
-                'Ah_V': delta_charge/delta_voltage,
+                'Ah_V': Ah_V,
                 'SoC': np.nan
             }
 
@@ -95,6 +116,73 @@ class RandomProcedure(Procedure):
             if self.should_stop():
                 log.warning("Caught the stop flag in the procedure")
                 break
+        
+        charge = 0.0
+
+        if self.should_stop():
+            log.warning("Skipping recharge")
+        else:
+
+            log.info('Charging at 3.4A')
+            self.boss.write('PC+3.400')
+            log.info(self.boss.read())
+
+            time_elapsed = 0
+
+            buffer_empty = False
+            while buffer_empty == False:
+                try:
+                    log.info(self.boss.read())
+                except:
+                    log.info('Buffer empty')
+                    buffer_empty = True
+                else:
+                    log.info('Purged 1 line from buffer')
+                    log.info('Residue in buffer')
+
+            while time_elapsed < timeout_seconds * 2:
+                voltage = self.boss.query_ascii_values('MV')[0]
+                log.info(self.boss.read())
+                current = self.boss.query_ascii_values('MI')[0]
+                log.info(self.boss.read())
+
+                time_elapsed = perf_counter() - test_start_time
+                time_interval = time_elapsed - last_time
+                delta_charge = time_interval / 3600 * current
+                charge += delta_charge
+                last_time = time_elapsed
+                delta_voltage = voltage - last_voltage
+                last_voltage = voltage
+                if delta_voltage == 0.0:
+                    Ah_V = np.nan
+                else:
+                    Ah_V = delta_charge/delta_voltage
+
+                data = {
+                    'Time': time_elapsed,
+                    'Voltage': voltage,
+                    'Current': current,
+                    'Charge': charge,
+                    'Ah_V': Ah_V,
+                    'SoC': charge / self.nominal_capacity
+                }
+
+                self.emit('results', data)
+                log.debug("Emitting results: %s" % data)
+                self.emit('progress', 100 * time_elapsed / timeout_seconds)
+
+                if voltage >= self.charge_voltage:
+                    log.info('Pack charged')
+                    break
+
+                sleep(0.5)
+                if self.should_stop():
+                    log.warning("Caught the stop flag in the procedure")
+                    break
+        
+        log.info('Setting current to zero')
+        self.boss.write('PC0')
+        log.info(self.boss.read())
 
 class MainWindow(ManagedWindow):
 
@@ -102,8 +190,8 @@ class MainWindow(ManagedWindow):
         super().__init__(
             procedure_class=RandomProcedure,
             inputs=['nominal_capacity', 'charge_rate', 'discharge_rate', 'charge_voltage', 'discharge_voltage'],
-            displays=['nominal_capacity', 'charge_rate', 'discharge_rate', 'charge_voltage', 'discharge_voltage'],,
-            x_axis='Elapsed time',
+            displays=['nominal_capacity', 'charge_rate', 'discharge_rate', 'charge_voltage', 'discharge_voltage'],
+            x_axis='Time',
             y_axis='Voltage'
         )
         self.setWindowTitle('Battery Tester')
