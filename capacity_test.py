16fbd8497eeea9cf4cc0e86d46716fec9fd70626
Matt G <garmat@gmail.com>
2023-05-29 17:37:57 -0400

    charge, float, discharge

--- capacity_test.py
+++ capacity_test.py
@@ -15,11 +15,13 @@ from pymeasure.experiment import IntegerParameter, FloatParameter, Parameter
 
 class RandomProcedure(Procedure):
 
-    nominal_capacity = FloatParameter('Nominal', units='Ah', default=17)
-    charge_rate = FloatParameter('Charge rate', units='C', default=0.2)
-    discharge_rate = FloatParameter('Discharge rate', units='C', default=0.2)
-    charge_voltage = FloatParameter('Charge limit', units='V', default=4.00)
-    discharge_voltage = FloatParameter('Discharge limit', units='V', default=3.65)
+    nominal_capacity = FloatParameter('Nominal', units='Ah', default=25.3)
+    charge_rate = FloatParameter('Charge rate', units='C', default=0.5)
+    discharge_rate = FloatParameter('Discharge rate', units='C', default=0.5)
+    charge_voltage = FloatParameter('Charge limit', units='V', default=3.8)
+    float_charge_voltage  = FloatParameter('Float voltage', units='V', default=3.45)
+    float_current_cutoff = FloatParameter('Float cutoff', units='A', default=0.55)
+    discharge_voltage = FloatParameter('Discharge limit', units='V', default=2)
 
     DATA_COLUMNS = ['Time', 'Voltage', 'Current', 'Charge', 'Ah_V', 'SoC']
 
@@ -35,120 +37,62 @@ class RandomProcedure(Procedure):
 
         log.info('Setting Remote mode')
         self.boss.write('SR')
-        log.info(self.boss.read_raw())
-        log.info(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
 
         log.info('Program limit to 1/4 ful scale (0x40)')
         self.boss.write('PL+40')
-        log.info(self.boss.read_raw())
-        log.info(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
 
         log.info('Set current control mode')
         self.boss.write('SI')
-        log.info(self.boss.read_raw())
-        log.info(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
 
         log.info('Zero setpoint')
         self.boss.write('PC0')
-        log.info(self.boss.read_raw())
-        log.info(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
 
     def execute(self):
-        log.info('Discharging at 3.4A')
-        self.boss.write('PC-3.400')
-        log.info(self.boss.read_raw())
-        log.info(self.boss.read_raw())
-
         test_start_time = perf_counter()
         time_elapsed = 0
-        last_time = test_start_time
-        log.info("Clock time: %f" % test_start_time)
-        timeout_seconds = 6*3600
+        last_time = 0
         charge = 0.0
         last_voltage = 0.0
-
-        buffer_empty = False
-        while buffer_empty == False:
-            try:
-                log.info(self.boss.read())
-            except:
-                log.info('Buffer empty')
-                buffer_empty = True
-            else:
-                log.info('Purged 1 line from buffer')
-                log.info('Residue in buffer')
-
-        while time_elapsed < timeout_seconds:
-            voltage = self.boss.query_ascii_values('MV')[0]
-            log.info(self.boss.read())
-            current = self.boss.query_ascii_values('MI')[0]
-            log.info(self.boss.read())
-
-            time_elapsed = perf_counter() - test_start_time
-            time_interval = time_elapsed - last_time
-            delta_charge = current * time_interval / 3600
-            charge += delta_charge
-            last_time = time_elapsed
-            delta_voltage = voltage - last_voltage
-            if delta_voltage == 0.0:
-                Ah_V = np.nan
-            else:
-                Ah_V = delta_charge/delta_voltage
-
-            data = {
-                'Time': time_elapsed,
-                'Voltage': voltage,
-                'Current': current,
-                'Charge': charge,
-                'Ah_V': Ah_V,
-                'SoC': np.nan
-            }
-
-            self.emit('results', data)
-            log.debug("Emitting results: %s" % data)
-            self.emit('progress', 100 * time_elapsed / timeout_seconds)
-
-            if voltage <= self.discharge_voltage:
-                log.info('Pack discharged')
-                break
-
-            sleep(0.5)
-            if self.should_stop():
-                log.warning("Caught the stop flag in the procedure")
-                break
-        
-        charge = 0.0
+        timeout_seconds = self.nominal_capacity * 3600 / self.charge_rate
 
         if self.should_stop():
             log.warning("Skipping recharge")
         else:
-
-            log.info('Charging at 3.4A')
-            self.boss.write('PC+3.400')
-            log.info(self.boss.read())
+            log.info(f'Charging at {self.charge_rate * self.nominal_capacity:.3n}A')
+            self.boss.write(f'PC+{self.charge_rate * self.nominal_capacity:.3f}A')
+            log.debug(self.boss.read())
 
             time_elapsed = 0
+            last_time = 0
 
             buffer_empty = False
             while buffer_empty == False:
                 try:
-                    log.info(self.boss.read())
+                    log.debug(self.boss.read())
                 except:
-                    log.info('Buffer empty')
+                    log.debug('Buffer empty')
                     buffer_empty = True
                 else:
-                    log.info('Purged 1 line from buffer')
-                    log.info('Residue in buffer')
+                    log.debug('Purged 1 line from buffer')
+                    log.debug('Residue in buffer')
 
             while time_elapsed < timeout_seconds * 2:
                 voltage = self.boss.query_ascii_values('MV')[0]
-                log.info(self.boss.read())
+                log.debug(self.boss.read())
                 current = self.boss.query_ascii_values('MI')[0]
-                log.info(self.boss.read())
+                log.debug(self.boss.read())
 
                 time_elapsed = perf_counter() - test_start_time
                 time_interval = time_elapsed - last_time
-                delta_charge = time_interval / 3600 * current
+                delta_charge = current * time_interval / 3600
                 charge += delta_charge
                 last_time = time_elapsed
                 delta_voltage = voltage - last_voltage
@@ -164,7 +108,7 @@ class RandomProcedure(Procedure):
                     'Current': current,
                     'Charge': charge,
                     'Ah_V': Ah_V,
-                    'SoC': charge / self.nominal_capacity
+                    'SoC': 100 * charge / self.nominal_capacity
                 }
 
                 self.emit('results', data)
@@ -175,14 +119,154 @@ class RandomProcedure(Procedure):
                     log.info('Pack charged')
                     break
 
-                sleep(0.5)
+                sleep(2)
+                if self.should_stop():
+                    log.warning("Caught the stop flag in the procedure")
+                    break
+        
+        if self.should_stop():
+            log.warning("Skipping float charge")
+        else:
+            log.info('Set to voltage control mode')
+            self.boss.write('SV')
+            log.debug(self.boss.read_raw())
+            log.debug(self.boss.read_raw())
+            log.info('Program current limit to 1/2 ful scale (0x80)')
+            self.boss.write('PL+80')
+
+            log.debug(self.boss.read_raw())
+            log.debug(self.boss.read_raw())
+            log.info(f'Float charging at {self.float_charge_voltage:.3n} V')
+            self.boss.write(f'PC+{self.float_charge_voltage:.3f}V')
+            log.debug(self.boss.read())
+
+            time_elapsed = 0
+
+            buffer_empty = False
+            while buffer_empty == False:
+                try:
+                    log.debug(self.boss.read())
+                except:
+                    log.debug('Buffer empty')
+                    buffer_empty = True
+                else:
+                    log.debug('Purged 1 line from buffer')
+                    log.debug('Residue in buffer')
+
+            float_timer = 0
+            float_seconds = 30 * 60
+
+            while float_timer < float_seconds:
+                voltage = self.boss.query_ascii_values('MV')[0]
+                log.debug(self.boss.read())
+                current = self.boss.query_ascii_values('MI')[0]
+                log.debug(self.boss.read())
+
+                time_elapsed = perf_counter() - test_start_time
+                time_interval = time_elapsed - last_time
+                delta_charge = current * time_interval / 3600
+                charge += delta_charge
+                last_time = time_elapsed
+                delta_voltage = voltage - last_voltage
+                last_voltage = voltage
+                if delta_voltage == 0.0:
+                    Ah_V = np.nan
+                else:
+                    Ah_V = delta_charge/delta_voltage
+
+                data = {
+                    'Time': time_elapsed,
+                    'Voltage': voltage,
+                    'Current': current,
+                    'Charge': charge,
+                    'Ah_V': Ah_V,
+                    'SoC': np.nan #100 * charge / self.nominal_capacity
+                }
+
+                self.emit('results', data)
+                log.debug("Emitting results: %s" % data)
+                self.emit('progress', 100 * float_timer / float_seconds)
+
+                if current <= self.float_current_cutoff:
+                    log.info('Pack charged')
+                    break
+
+                sleep(2)
+                if self.should_stop():
+                    log.warning("Caught the stop flag in the procedure")
+                    break
+        
+        if self.should_stop():
+            log.warning("Skipping discharge test")
+        else:
+            log.info(f'Discharging at {self.discharge_rate * self.nominal_capacity:.3n}A')
+            self.boss.write(f'PC-%{self.discharge_rate * self.nominal_capacity:.3f}')
+            log.debug(self.boss.read_raw())
+            log.debug(self.boss.read_raw())
+
+            time_elapsed = 0
+            last_time = 0
+            log.info("Clock time: %f" % test_start_time)
+            
+
+
+            buffer_empty = False
+            while buffer_empty == False:
+                try:
+                    log.debug(self.boss.read())
+                except:
+                    log.debug('Buffer empty')
+                    buffer_empty = True
+                else:
+                    log.debug('Purged 1 line from buffer')
+                    log.debug('Residue in buffer')
+
+            while time_elapsed < timeout_seconds:
+                voltage = self.boss.query_ascii_values('MV')[0]
+                log.debug(self.boss.read())
+                current = self.boss.query_ascii_values('MI')[0]
+                log.debug(self.boss.read())
+
+                time_elapsed = perf_counter() - test_start_time
+                time_interval = time_elapsed - last_time
+                delta_charge = current * time_interval / 3600
+                charge += delta_charge
+                last_time = time_elapsed
+                delta_voltage = voltage - last_voltage
+                if delta_voltage == 0.0:
+                    Ah_V = np.nan
+                else:
+                    Ah_V = delta_charge/delta_voltage
+
+                data = {
+                    'Time': time_elapsed,
+                    'Voltage': voltage,
+                    'Current': current,
+                    'Charge': charge,
+                    'Ah_V': Ah_V,
+                    'SoC': 100 * (1 - (charge / self.nominal_capacity))
+                }
+
+                self.emit('results', data)
+                log.debug("Emitting results: %s" % data)
+                self.emit('progress', 100 * time_elapsed / timeout_seconds)
+
+                if voltage <= self.discharge_voltage:
+                    log.info('Pack discharged')
+                    break
+
+                sleep(2)
                 if self.should_stop():
                     log.warning("Caught the stop flag in the procedure")
                     break
         
         log.info('Setting current to zero')
+        self.boss.write('SI')
+        log.debug(self.boss.read_raw())
+        log.debug(self.boss.read_raw())
         self.boss.write('PC0')
         log.info(self.boss.read())
+        log.debug(self.boss.read_raw())
 
 class MainWindow(ManagedWindow):
 
