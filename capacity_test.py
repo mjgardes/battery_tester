ba583a89e4502d5402bfcd00aa2b7fec73b065eb
Matt G <garmat@gmail.com>
2023-05-29 20:25:54 -0400

    math is hard

--- capacity_test.py
+++ capacity_test.py
@@ -205,7 +205,7 @@ class RandomProcedure(Procedure):
             log.debug(self.boss.read_raw())
 
             discharge_start = time_elapsed
-            last_time = perf_counter()
+            last_time = perf_counter() - test_start_time
             charge = 0.0
             log.info("Clock time: %f" % test_start_time)
             
