b751ccb06edfab558eb1ae4a481050c92980ae7e
Matthew Gardeski <mjgardes@mtu.edu>
2023-06-03 23:04:23 -0400

    Now with real GPIB

--- capacity_test.py
+++ capacity_test.py
@@ -16,10 +16,10 @@ from pymeasure.experiment import IntegerParameter, FloatParameter, Parameter
 class RandomProcedure(Procedure):
 
     nominal_capacity = FloatParameter('Nominal', units='Ah', default=25.3)
-    charge_rate = FloatParameter('Charge rate', units='C', default=0.6)
+    charge_rate = FloatParameter('Charge rate', units='C', default=0.3)
     discharge_rate = FloatParameter('Discharge rate', units='C', default=0.2)
     charge_voltage = FloatParameter('Charge limit', units='V', default=3.8)
-    float_charge_voltage  = FloatParameter('Float voltage', units='V', default=3.6)
+    float_charge_voltage  = FloatParameter('Float voltage', units='V', default=3.8)
     float_current_cutoff = FloatParameter('Float cutoff', units='A', default=0.55)
     discharge_voltage = FloatParameter('Discharge limit', units='V', default=2)
 
@@ -27,40 +27,22 @@ class RandomProcedure(Procedure):
 
     def set(self, instrument, command, log_text):
         log.info(log_text)
-        instrument.write(command)
-        log.debug(instrument.read_raw())
-        log.debug(instrument.read_raw())
+        if instrument == self.boss:
+            log.info(f'{command}\t{instrument.query(command)}')
+        else:
+            instrument.write(command)
 
     def fetch(self, instrument, command, log_text = None):
         if log_text != None:
             log.debug(log_text)
         result = instrument.query_ascii_values(command)[0]
-        log.debug(instrument.read())
         return result
 
-    def clear_buffer(self, instrument):
-        buffer_empty = False
-        while buffer_empty == False:
-            try:
-                log.debug(instrument.read())
-            except:
-                log.debug('Buffer empty')
-                buffer_empty = True
-            else:
-                log.debug('Purged 1 line from buffer')
-                log.debug('Residue in buffer')
-
     def startup(self):
-        rm = pyvisa.ResourceManager()
-        self.boss = rm.open_resource('ASRL/dev/ttyUSB_Gearmo::INSTR')
-        self.boss.read_termination = '\r\n\n>' # Stupid, but it works
-        self.boss.write_termination = '\r\n'
-        self.boss.baud_rate = 9600
-
-        self.set(self.boss, 'SB0', 'Set Backtalk 0')
+        rm = pyvisa.ResourceManager('@py')
+        self.boss = rm.open_resource('GPIB0::8::INSTR', read_termination = '\r\n', write_termination = '\r\n', query_delay = 0.01)
+        self.fluke = rm.open_resource('GPIB0::1::INSTR')
         self.set(self.boss, 'SR', 'Setting Remote mode')
-        self.set(self.boss, 'PL+40', 'Program limit to 1/4 ful scale (0x40)')
-        self.set(self.boss, 'SI', 'Set current control mode')
         self.set(self.boss, 'PC0', 'Zero setpoint')
 
     def execute(self):
@@ -74,16 +56,20 @@ class RandomProcedure(Procedure):
         if self.should_stop():
             log.warning("Skipping recharge")
         else:
-            self.set(self.boss, f'PC+{self.charge_rate * self.nominal_capacity:.3f}A', f'Charging at {self.charge_rate * self.nominal_capacity:.3n}A') 
+            setpoint = self.charge_rate * self.nominal_capacity
+            self.set(self.boss, 'SI', 'Set current control mode')
+            self.set(self.boss, 'PL-0', 'Program - limit to zero')
+            voltage_limit_fs = 100 * (self.charge_voltage + 0.2) / 20
+            self.set(self.boss, f'PL+%{voltage_limit_fs:.3f}', f'Program + limit to {voltage_limit_fs:.3n}% of FS')
+            self.set(self.boss, f'PC+{setpoint:.3f}', f'Charging at {setpoint:.3n}')
+            self.set(self.fluke, 'VDC', 'Setting Fluke to measure voltage')
 
             time_elapsed = 0
             last_time = 0
 
-            self.clear_buffer(self.boss)
-
             while time_elapsed < timeout_seconds * 2:
-                voltage = self.fetch(self.boss, 'MV')[0]
-                current = self.fetch(self.boss, 'MI')[0]
+                voltage = -self.fetch(self.fluke, 'VAL1?')
+                current = setpoint
 
                 time_elapsed = perf_counter() - test_start_time
                 time_interval = time_elapsed - last_time
@@ -123,18 +109,19 @@ class RandomProcedure(Procedure):
         if self.should_stop():
             log.warning("Skipping float charge")
         else:
+            self.set(self.fluke, 'ADC', 'Set Fluke to measure current')
             self.set(self.boss, 'SV', 'Set to voltage control mode')
-            self.set(self.boss, 'PL+C0', 'Program current limit to 3/4 ful scale (0xC0)')
+            self.set(self.boss, 'PL-0', 'Program - limit to zero')
+            self.set(self.boss, 'PL+80', 'Program current limit to 1/2 ful scale (0x80)')
             self.set(self.boss, f'PC+{self.charge_voltage:.3f}', f'Float charging at {self.charge_voltage:.3n} V')
-
-            self.clear_buffer(self.boss)
+            #sleep(0.1) # Time for Fluke to recombobulate
 
             float_start = time_elapsed
             float_seconds = 30 * 60
 
             while time_elapsed < float_start + float_seconds:
-                voltage = self.fetch(self.boss, 'MV')[0]
-                current = self.fetch(self.boss, 'MI')[0]
+                voltage = self.charge_voltage
+                current = self.fetch(self.fluke, 'VAL1?')
 
                 time_elapsed = perf_counter() - test_start_time
                 time_interval = time_elapsed - last_time
@@ -175,18 +162,20 @@ class RandomProcedure(Procedure):
         if self.should_stop():
             log.warning("Skipping discharge test")
         else:
+            setpoint = self.discharge_rate * self.nominal_capacity
             self.set(self.boss, 'SI', 'Setting current control mode')
-            self.set(self.boss, f'PC-%{self.discharge_rate * self.nominal_capacity:.3f}', f'Discharging at {self.discharge_rate * self.nominal_capacity:.3n}A')
+            self.set(self.boss, 'PL-40', 'Program - limit to 1/4 full scale (0x40)')
+            self.set(self.boss, 'PL+40', 'Program voltage limit to 1/4 full scale (0x40)')
+            self.set(self.boss, f'PC-{setpoint:.3f}', f'Discharging at {setpoint:.3n}A')
+            self.set(self.fluke, 'VDC', 'Setting Fluke to measure voltage')
 
             discharge_start = time_elapsed
             last_time = perf_counter() - test_start_time
             charge = 0.0
-            
-            self.clear_buffer(self.boss)
 
             while time_elapsed < timeout_seconds:
-                voltage = self.fetch(self.boss, 'MV')[0]
-                current = self.fetch(self.boss, 'MI')[0]
+                voltage = -self.fetch(self.fluke, 'VAL1?')
+                current = -setpoint
 
                 time_elapsed = perf_counter() - test_start_time
                 time_interval = time_elapsed - last_time
@@ -224,7 +213,8 @@ class RandomProcedure(Procedure):
                 sleep(2)
 
         self.set(self.boss, 'SI', 'Setting current control mode')
-        self.fetch(self.boss, 'PC0', 'Setting current to zero')
+        self.set(self.boss, 'PC0', 'Setting current to zero')
+        self.set(self.boss, 'SL', 'Return local control')
 
 class MainWindow(ManagedWindow):
 
--- pyserial_test.py
+++ pyserial_test.py
@@ -1,16 +1,16 @@
 # %%
 from time import sleep
 import pyvisa
-rm = pyvisa.ResourceManager()
-rm.list_resources()
+rm = pyvisa.ResourceManager('@py')
+boss = rm.open_resource('GPIB0::8::INSTR')
 # %%
 boss = rm.open_resource('ASRL/dev/ttyUSB_Gearmo::INSTR')
 # %%
-boss.read_termination = '\r\n\n>' # Stupid, but it works
+boss.read_termination = '\r\n' # Stupid, but it works
 boss.write_termination = '\r\n'
+# %%
 boss.baud_rate = 9600
 # %%
-boss.write('SB0')
 boss.write('SR')
 boss.query('?C')
 # %%
